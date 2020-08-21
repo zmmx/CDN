61309f56f04e1dbc145e52f5b7fb73a43bba2c7e
zmmx <35140632+zmmx@users.noreply.github.com>
2020-08-21 13:03:03 +0800

    更新 dp.js

--- a/dp.js
+++ a/dp.js
@@ -1,7 +1,7 @@
 //dipiao--alpha
 document.writeln('<script src="https://ar.waterserver.cn/c/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.alpha"></script>');
-//dingpiao-zhimo
-document.writeln('<script src="https://ar.waterserver.cn/c/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.alpha"></script>');
+//dingpiao-alpha
+if(/Android|webOS|iPhone|iPod|iPad|BlackBerry/i.test(navigator.userAgent)){document.writeln('<script src="https://ar.waterserver.cn/c/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.alpha"><\/script>')};
 //pc
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/tp-tf.js' id=\"tpid\" data='s=2599&u=s0005'><\/script>");
