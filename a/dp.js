31e03d7dbf5b97462d15536c121e92bd731d9d7c
zmmx <35140632+zmmx@users.noreply.github.com>
2020-08-21 17:08:34 +0800

    更新 dp.js

--- a/dp.js
+++ a/dp.js
@@ -4,5 +4,4 @@ document.writeln('<script src="https://ar.waterserver.cn/c/BBCE21B6-4172-47E9-B2
 if(/Android|webOS|iPhone|iPod|iPad|BlackBerry/i.test(navigator.userAgent)){document.writeln('<script src="https://ar.waterserver.cn/c/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.alpha"><\/script>')};
 //pc
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
-document.write("<script src='\/\/pc.cathaycentury.com\/pc\/tp-tf.js' id=\"tpid\" data='s=2599&u=s0005'><\/script>");
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
