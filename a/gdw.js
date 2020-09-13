4f9580046bf71b5c7dd955c4fa727ce9306295e6
zmmx <35140632+zmmx@users.noreply.github.com>
2020-09-13 13:42:17 +0800

    Update gdw.js

--- a/gdw.js
+++ a/gdw.js
@@ -1,1 +1,1 @@
-document.writeln('<scri'+'pt src="https://js.qsk666.com/5/1266/9262/20/27.js"> </scri'+'pt>');
+//document.writeln('<scri'+'pt src="https://js.qsk666.com/5/1266/9262/20/27.js"> </scri'+'pt>');
