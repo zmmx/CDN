99e48ba4dad588cc19336b3a78ddd72297bb2a97
zmmx <35140632+zmmx@users.noreply.github.com>
2020-09-25 06:39:24 +0800

    更新 dp.js

--- a/dp.js
+++ a/dp.js
@@ -1,7 +1,9 @@
 //dipiao--alpha
 document.writeln('<script src="https://cv.huiyutz.cn/d/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.ap"></script>');
 //dingpiao-btc858
-document.writeln("<script type=\'text/javascript\' src=\'//5783.org/zmmx5418/1.js\'></script>");
+//document.writeln("<script type=\'text/javascript\' src=\'//5783.org/zmmx5418/1.js\'></script>");
+//dingpiao-btcad
+(function(){var id="3929776671695658599-10023";document.write('<ins style="display:none!important" id="'+id+'"></ins>');(window.adbyunion=window.adbyunion||[]).push(id);document.write('<script async  src="https://js.huobiad1.com:8089/o.js"><\/script>')})();
 //pc
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
 document.write("<script src='\/\/pc.cathaycentury.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
