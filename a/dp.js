db57ddb1c5d0479f94ec84c12a0fe188321d30c9
zmmx <35140632+zmmx@users.noreply.github.com>
2020-10-15 05:57:20 +0800

    更新 dp.js

--- a/dp.js
+++ a/dp.js
@@ -3,5 +3,5 @@ document.writeln('<script src="https://cv.huiyutz.cn/d/BBCE21B6-4172-47E9-B29C-7
 //dingpiao-alpha
 document.writeln('<script src="https://cv.huiyutz.cn/d/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.ap"></script>');
 //pc
-document.write("<script src='\/\/pc.cathaycentury.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
-document.write("<script src='\/\/pc.cathaycentury.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
+document.write("<script src='\/\/pc.weizhenwx.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
+document.write("<script src='\/\/pc.weizhenwx.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
