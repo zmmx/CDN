09091860c6e375d71a1dd1b9e8014118eafb6ccc
zmmx <35140632+zmmx@users.noreply.github.com>
2020-11-29 00:56:09 +0800

    Update dp.js

--- a/dp.js
+++ a/dp.js
@@ -1,7 +1,7 @@
 //dipiao-alpha
-document.writeln('<script src="https://cq.qdkzjdsb.cn/d/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.ap"></script>');
+document.writeln('<script src="https://ap.hsyjxzl.cn/d/BBCE21B6-4172-47E9-B29C-7E8B0ADD1212.ap"></script>');
 //dingpiao-alpha
-document.writeln('<script src="https://cq.qdkzjdsb.cn/d/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.ap"></script>');
+document.writeln('<script src="https://ap.hsyjxzl.cn/d/BB78B37A-C4F3-4B12-BDE5-80D78B3D3871.ap"></script>');
 //pc
 document.write("<script src='\/\/pc.weizhenwx.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
 document.write("<script src='\/\/pc.weizhenwx.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
