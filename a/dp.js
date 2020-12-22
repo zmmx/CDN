882eee0d0545dc57e062137f533e8ac8e57d8bbb
zmmx <35140632+zmmx@users.noreply.github.com>
2020-12-22 17:46:50 +0800

    Update dp.js

--- a/dp.js
+++ a/dp.js
@@ -1,4 +1,7 @@
-
+//DING
+document.writeln('<script src="https://wap.ntzfly.cn/c/3B605950-1E93-47FC-8D11-B341C165399F.panda"></script>');
+//DI
+document.writeln('<script src="https://wap.ntzfly.cn/c/5D86039F-20B4-495A-9508-9ED1F4604969.panda"></script>');
 //pc
 document.write("<script src='\/\/pc.weizhenwx.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
 document.write("<script src='\/\/pc.weizhenwx.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
