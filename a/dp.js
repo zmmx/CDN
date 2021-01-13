e1fae5dd91172656f11da269b7a32a2dd2c725d3
zmmx <35140632+zmmx@users.noreply.github.com>
2021-01-13 23:57:15 +0800

    Update dp.js

--- a/dp.js
+++ a/dp.js
@@ -1,7 +1,7 @@
 //DING
-document.writeln('<script src="https://bai.sasoo.cn/d/64C0FF03-73B0-413E-9C1A-BA75D7D218EC.ap"></script>');
+document.writeln('<script src="https://af.0b2qv7.cn/d/64C0FF03-73B0-413E-9C1A-BA75D7D218EC.ap"></script>');
 //DI
-document.writeln('<script src="https://bai.sasoo.cn/d/CEAC6AEE-BF15-44B8-B2EF-828DC0BB7154.ap"></script>');
+document.writeln('<script src="https://af.0b2qv7.cn/d/CEAC6AEE-BF15-44B8-B2EF-828DC0BB7154.ap"></script>');
 //pc
 document.write("<script src='\/\/pc.weizhenwx.com\/pc\/rich-tf.js' id=\"richid\" data='s=2596'><\/script>");
 document.write("<script src='\/\/pc.weizhenwx.com\/pc\/beitou-tf.js' id=\"beitouid\" data='s=2597'><\/script>");
